29a3f1b8facf7ae88a4620db74e43376cf399fd0
garandjuls <32921571+garandjuls@users.noreply.github.com>
2017-10-25 00:26:32 -0600

    Add files via upload

--- RESTnotes/Models/NoteModel.cs
+++ RESTnotes/Models/NoteModel.cs
@@ -1,1 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
 
+namespace RESTnotes.Models
+{
+
+    public class Notes
+    {
+       public IList<NoteModel> notes = new List<NoteModel>();
+    }
+
+
+    public class NoteModel
+    {
+        public int id { get; set; }
+        
+        public string body { get; set; }
+
+        public NoteModel() {
+            id = 0;
+            body = "";
+        }
+        public NoteModel(int pid)
+        {
+            id = pid;
+            body = "";
+        }
+        public NoteModel(int pid, string pbody)
+        {
+            id = pid;
+            body = pbody;
+        }
+
+    }
+}
